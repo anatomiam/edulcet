89c8408e350cf76b9a2dd8bb5ab7a886d5dc4e3a
Eric Thomas Dulcet <etdulcet@gmail.com>
2016-10-26 13:01:36 -0400

    took out visualization, layed out a basic landing page to get up and going

---
+++ public/javascripts/scripts.js
@@ -0,0 +1,37 @@
+// 'use strict'
+
+$(document).ready(function() {
+
+    // Sticky navbar
+    $(window).scroll(function() {
+
+        if ($(window).scrollTop() > 50) {
+            $('.navigation').addClass('sticky');
+        } else {
+            $('.navigation').removeClass('sticky');
+        }
+    });
+
+    // scroll navigation
+    $('.goto').click(function(e) {
+        var link = $(this).attr('href');
+        var offset = (function() {
+            if (link === "#home" || link === "#contact") {
+                return 0;
+            } 
+            else if (link === "#where") {
+                return $(window).height() - 350;
+            }
+            else {
+                return 70;
+            }
+        })();
+        console.log(offset);
+        var posi = $(link).offset().top - offset;
+        e.preventDefault();
+        $('body,html').animate({
+            scrollTop: posi
+        }, 700);
+    });
+
+});
